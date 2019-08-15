60e343d1508c90cd44105b4ba51fa569250988bf
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-15 17:10:24 +0100

    cleanup in the public folder and create src folder from scratch

---
+++ src/component/app/app.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MadCreation = () => {
+  return <h1>This is my mind gone MAAAAADDD.....</h1>;
+};
+
+export default MadCreation;
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import MadCreation from './component/app';
+
+const renderBlock = document.querySelector('#renderMyCreation');
+
+render(<MadCreation />, renderBlock);
