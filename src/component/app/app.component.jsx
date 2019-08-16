fcd7bfe03d5975397f2ce745c7027ab0a2d1e7d4
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-16 11:43:31 +0100

    create picture component

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import ImageGallery from '../image-gallery/image-gallery.component';
+import Picture from '../picture';
 
 const MadCreation = () => {
-  return <h1>This is my mind gone MAAAAADDD.....</h1>;
+  return (
+    <div>
+      <h1>This is my mind gone MAAAAADDD.....</h1>
+      <ImageGallery />
+      <Picture />
+    </div>
+  );
 };
 
 export default MadCreation;
